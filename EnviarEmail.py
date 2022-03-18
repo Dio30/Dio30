857b72ce88619b2dcfd4f3645741989dd533e08a
Dio30 <101674106+Dio30@users.noreply.github.com>
2022-03-17 23:41:54 -0300

    Como vincular Python com excel e enviar email

---
+++ EnviarEmail.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import win32com.client as win32
+
+
+# Abrir os 6 arquivos em excel
+lista_meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
+
+for mes in lista_meses:
+    tabela_vendas = pd.read_excel(f'{mes}.xlsx')
+    if (tabela_vendas['Vendas'] > 55000).any():
+        vendedor = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendedor'].values[0]
+        vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendas'].values[0]
+        print(f'No mês {mes} alguém bateu a meta. Vendedor: {vendedor}, Vendas: {vendas}')
+
+outlook = win32.Dispatch('outlook.application')
+
+email = outlook.CreateItem(0)
+email.To = "padilhadione1@gmail.com"
+email.Subject = "Atingimento de Meta"
+email.HTMLBody = f"""
+<p>Segue quem atingiu a meta e qual valor atingido</p>
+
+<p>No mês {mes} alguém bateu a meta. Vendedor: {vendedor}, Vendas: {vendas}</p>
+
+<p>ATT,</p>
+<p>Dione R Padilha</p>
+"""
+
+email.Send()
+print("Email Enviado")
+
+# Para cada arquivo:
+
+# Verificar se algum valor na coluna de vendas daquele arquivo é maior que 55000
+
+# Se for maior que 55000 enviar SMS com o nome, mes e as vendas do vendedor
